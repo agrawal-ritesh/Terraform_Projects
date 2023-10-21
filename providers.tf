3c8c6b39cf3c485e943e1e0be2166bb2300df8a8
Ritesh DevOps <riteshagarwal1497@gmail.com>
2023-10-21 23:42:15 +0530

    Terraform Providers

---
+++ providers.tf
@@ -0,0 +1,14 @@
+terraform{
+    required_providers{
+        azurerm ={
+            source = "hashicorp/azurerm"
+            version = "3.77.0"
+
+        }
+    }
+}
+provider "azurerm" {
+    subscription_id = "6883dfc1-f1f6-41c4-a578-5dcbd9ef10e0"
+    tenant_id ="a70bb139-71ea-4653-b99f-fc3ca97b02fe"
+
+}
